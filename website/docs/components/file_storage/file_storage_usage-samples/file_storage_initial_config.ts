f625df6c5f9a8dc38c29a768682497478c37f456
Yousif Abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2026-09-12 01:44:22 +0200

    docs(file-storage): remove unused TimeSpan import from sample

--- website/docs/components/file_storage/file_storage_usage-samples/file_storage_initial_config.ts
+++ website/docs/components/file_storage/file_storage_usage-samples/file_storage_initial_config.ts
@@ -1,4 +1,3 @@
-import { TimeSpan } from "eridu-tech/time-span";
 import { MemoryFileStorageAdapter } from "eridu-tech/file-storage/memory-file-storage-adapter";
 import { SignedFileStorageAdapter } from "eridu-tech/file-storage/signed-file-storage-adapter";
 import { FileStorage } from "eridu-tech/file-storage";
