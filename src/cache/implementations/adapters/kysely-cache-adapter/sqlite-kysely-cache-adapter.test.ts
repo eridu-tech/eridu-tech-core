1a1bc7d7c62e4a74a78e8eb152bde401bb518413
Yousif Abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2026-09-16 02:47:18 +0200

    test: Add init and deInit tests to Kysely cache adapters

--- src/cache/implementations/adapters/kysely-cache-adapter/sqlite-kysely-cache-adapter.test.ts
+++ src/cache/implementations/adapters/kysely-cache-adapter/sqlite-kysely-cache-adapter.test.ts
@@ -13,12 +13,14 @@ import { KyselyTransactionAdapter } from "@/transaction-context/implementations/
 import { TransactionContext } from "@/transaction-context/implementations/derivables/_module.js";
 
 import type { Database } from "better-sqlite3";
+import type { ColumnMetadata, TableMetadata } from "kysely";
 
 import type { KyselyCacheTables } from "@/cache/implementations/adapters/kysely-cache-adapter/_module.js";
 import type { ITransactionContext } from "@/transaction-context/contracts/_module.js";
 
 describe("sqlite class: KyselyCacheAdapter", () => {
     let database: Database;
+
     beforeEach(() => {
         database = new Sqlite(":memory:");
     });
@@ -56,6 +58,98 @@ describe("sqlite class: KyselyCacheAdapter", () => {
         beforeEach,
         expect,
         describe,
+    });
+    describe("method: init", () => {
+        test("Should create cache table", async () => {
+            const trxCtx = createTrxCtx(database);
+            const adapter = new KyselyCacheAdapter({
+                transactionContext: trxCtx,
+                serde: new Serde(new SuperJsonSerdeAdapter()),
+            });
+            await adapter.init();
+
+            const tables = await trxCtx.client.introspection.getTables();
+
+            expect(tables).toContainEqual(
+                expect.objectContaining<Partial<TableMetadata>>({
+                    name: "cache",
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument
+                    columns: expect.arrayContaining<Partial<ColumnMetadata>>([
+                        expect.objectContaining<Partial<ColumnMetadata>>({
+                            name: "key",
+                            dataType: "varchar(255)",
+                            // SQLite allows NULLs in non-integer primary keys, so `key` is nullable.
+                            isNullable: true,
+                            hasDefaultValue: false,
+                        }),
+                        expect.objectContaining<Partial<ColumnMetadata>>({
+                            name: "value",
+                            dataType: "varchar(255)",
+                            isNullable: false,
+                            hasDefaultValue: false,
+                        }),
+                        expect.objectContaining<Partial<ColumnMetadata>>({
+                            name: "expiration",
+                            dataType: "bigint",
+                            isNullable: true,
+                            hasDefaultValue: false,
+                        }),
+                    ]),
+                }),
+            );
+        });
+        test("Should not throw error when called multiple times", async () => {
+            const adapter = new KyselyCacheAdapter({
+                transactionContext: createTrxCtx(database),
+                serde: new Serde(new SuperJsonSerdeAdapter()),
+            });
+            await adapter.init();
+
+            const promise = adapter.init();
+
+            await expect(promise).resolves.toBeUndefined();
+        });
+    });
+    describe("method: deInit", () => {
+        test("Should remove cache table", async () => {
+            const trxCtx = createTrxCtx(database);
+            const adapter = new KyselyCacheAdapter({
+                transactionContext: trxCtx,
+                serde: new Serde(new SuperJsonSerdeAdapter()),
+            });
+            await adapter.init();
+            await adapter.deInit();
+
+            const tables = await trxCtx.client.introspection.getTables();
+
+            expect(tables).not.toContainEqual(
+                expect.objectContaining<Partial<TableMetadata>>({
+                    name: "cache",
+                }),
+            );
+        });
+        test("Should not throw error when called multiple times", async () => {
+            const adapter = new KyselyCacheAdapter({
+                transactionContext: createTrxCtx(database),
+                serde: new Serde(new SuperJsonSerdeAdapter()),
+            });
+            await adapter.init();
+            await adapter.deInit();
+
+            const promise = adapter.deInit();
+
+            await expect(promise).resolves.toBeUndefined();
+        });
+        test("Should not throw error when called before init", async () => {
+            const adapter = new KyselyCacheAdapter({
+                transactionContext: createTrxCtx(database),
+                serde: new Serde(new SuperJsonSerdeAdapter()),
+            });
+
+            const promise = adapter.deInit();
+
+            await expect(promise).resolves.toBeUndefined();
+        });
     });
     test("Transaction test", async () => {
         const trxCtx = createTrxCtx(database);
