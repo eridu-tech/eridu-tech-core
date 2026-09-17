9799ee525de565061fbe17e23f291a919ca1e7d5
Yousif Abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2026-09-18 00:04:51 +0200

    enhancement(transaction-context): Add MultiTransactionHooks derivable

--- src/transaction-context/implementations/derivables/_module-exports.ts
+++ src/transaction-context/implementations/derivables/_module-exports.ts
@@ -1,1 +1,2 @@
 export * from "@/transaction-context/implementations/derivables/transaction-context/_module.js";
+export * from "@/transaction-context/implementations/derivables/multi-transaction-hooks/_module.js";
---
+++ src/transaction-context/implementations/derivables/multi-transaction-hooks/_module.ts
@@ -0,0 +1,1 @@
+export * from "@/transaction-context/implementations/derivables/multi-transaction-hooks/multi-transaction-hooks.js";
