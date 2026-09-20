6cea414ed6647a1e1cb6ff15e4e70def9b54d0d6
Yousif Abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2026-09-20 21:44:00 +0200

    fix(collection): Add the documented but missing getOr method
    - Declare getOr on the ICollection and IAsyncCollection contracts
    
    - Implement getOr in IterableCollection, ListCollection and AsyncIterableCollection

--- src/collection/contracts/collection.contract.ts
+++ src/collection/contracts/collection.contract.ts
@@ -354,6 +354,29 @@ export interface ICollection<TInput = unknown> extends Iterable<TInput> {
     get(index: number): TInput | null;
 
     /**
+     * The `getOr` method returns the item by index. If the item is not found null will returned.
+     * @example
+     * ```ts
+     * import type { ICollection } from "eridu-tech/collection/contracts";
+     *
+     * // Assume the inputed collection is empty.
+     * function main(collection: ICollection<number>): void {
+     *   collection = collection.append([1, 4, 2, 8, -2]);
+     *
+     *   // Will be 2
+     *   collection.getOr(2, -1);
+     *
+     *   // Will be -1
+     *   collection.getOr(5, -1);
+     * }
+     * ```
+     */
+    getOr<TExtended = TInput>(
+        index: number,
+        defaultValue: Lazyable<TExtended>,
+    ): TInput | TExtended;
+
+    /**
      * The `getOrFail` method returns the item by index. If the item is not found an error will be thrown.
      * @throws {ItemNotFoundCollectionError}
      * @example
